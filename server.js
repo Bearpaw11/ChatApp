2e27fdfe1a02d0c2d516ed0c94d2204576796097
Chris Behrens <chrisbehrens@yahoo.com>
2020-03-27 22:26:08 -0700

    added code for chatroom

---
+++ server.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const http = require('http')
+const express = require('express');
+const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+
+const app= express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adminName ='ChatApp admin'
+
+io.on('connection', socket => {
+
+    socket.on('joinRoom', ({username, room}) => {
+
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);  
+
+        //welcomes user
+        socket.emit('message', formatMessage(adminName, 'Welcome to ChatApp'));
+    
+        //when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(adminName, `${user.username} has joined the chat`));
+
+        // send user and roo info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+   
+    //listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+    //when user disconnect
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+         if(user) {
+        io.to(user.room).emit('message', formatMessage(adminName,`${user.username} has left the chat`));
+         }
+
+         io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
