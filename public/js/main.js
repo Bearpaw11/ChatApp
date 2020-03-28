adf2ce5465b6a0c6184cb7eca26bc5d9c4eb3f92
Chris Behrens <chrisbehrens@yahoo.com>
2020-03-28 14:39:15 -0700

    updated time

--- public/js/main.js
+++ public/js/main.js
@@ -48,6 +48,7 @@ chatForm.addEventListener('submit', e => {
 function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
+  console.log(message.time)
   div.innerHTML = `<p class="meta">${message.username} <span>${moment(message.time).format('LT')}</span></p>
   <p class ="text">
   ${message.text}
